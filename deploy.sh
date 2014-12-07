672c60bd1364fef93164217d418e35b65201a0e7
anton <achidyaev@yandex.ru>
2014-12-07 20:05:48 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,9 @@ if [ correctKey = inputKey]
 	then
 		#git pull origin master:master --quiet --progress
 		git pull origin master:master --verbose
+		npm install
+		bower update --allow-root
+		npm run build
 		exit 0
 	else
 		echo 'error'
