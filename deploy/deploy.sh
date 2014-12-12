a4c03b5fa3e5d4cb50432b8095e6be7da0e53792
anton <achidyaev@yandex.ru>
2014-12-13 00:14:12 +0300

    d

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,10 +9,19 @@ inputKey=$1
 if [ $correctKey = $inputKey ]
 	then
 		cd ../ &&
+
+		echo 'git pull'
 		git pull origin master:master --quiet
+		
+		echo 'installing node packages'
 		npm install
+		
+		echo 'updating bower packages'
 		bower update --force-latest
+		
+		echo 'build production project'
 		npm run production-build
+		
 		echo 'Successful deploy'
 		exit 0
 	else
