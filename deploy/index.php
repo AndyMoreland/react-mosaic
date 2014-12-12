2efd615b2de8194d7244990d8f0381db3cb0edaf
anton <achidyaev@yandex.ru>
2014-12-12 23:57:42 +0300

    d

--- deploy/index.php
+++ deploy/index.php
@@ -4,5 +4,5 @@ echo 'deploy '.$deployKey;
 $output1 =  shell_exec('whoami');
 echo "<pre>".$output1."</pre>";
 
-$output = shell_exec('/bin/bash deploy.sh '.$deployKey);
+$output = shell_exec('sh deploy.sh '.$deployKey);
 echo "<pre>$output</pre>";
