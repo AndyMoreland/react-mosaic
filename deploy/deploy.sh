02c778ec4db695705a4ab97b71581dcaf3b24865
anton <achidyaev@yandex.ru>
2014-12-12 21:49:56 +0300

    d

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,15 +9,15 @@ inputKey=$1
 if [ $correctKey = $inputKey ]
 	then
 		cd ../
-		git pull origin master:master --quiet --progress
-		npm install
-		bower update
+		git pull origin master:master --quiet
+		#npm install
+		#bower update
 		# npm run build:
-		ulimit -n 10000 
-		npm run css 
-		npm run transform-js 
-		npm run browserify
-		
+		#ulimit -n 10000 
+		#npm run css 
+		#npm run transform-js 
+		#npm run browserify
+
 		exit 0
 	else
 		echo 'Wrong deploy key '
