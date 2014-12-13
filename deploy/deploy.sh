b96b56ada910cc024825390d654e5f58da5833f8
anton <achidyaev@yandex.ru>
2014-12-13 11:57:00 +0300

    d

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,30 +2,29 @@
 #
 # Deploy script
 #
-git pull
+
 # correctKey=$(cat deployKey.txt)
 # inputKey=$1
 
 # if [ $correctKey = $inputKey ]
 # 	then
-# 		cd ../
+		cd ../
 
-# 		echo 'git pull'
-# 		commandGit=$(git pull)
-# 		echo $commandGit
+		echo 'git pull'
+		git pull
 		
-# 		echo 'installing node packages'
-# 		npm install
+		echo 'installing node packages'
+		npm install
 		
-# 		echo 'updating bower packages'
-# 		bower update --force-latest
+		echo 'updating bower packages'
+		bower update --force-latest
 		
-# 		echo 'build production project'
-# 		npm run production-build
+		echo 'build production project'
+		npm run production-build
 		
-# 		echo 'Successful deploy'
-# 		exit 0
-# 	else
-# 		echo 'Wrong deploy key'
-# 		exit 1
-# 	fi
+		echo 'Successful deploy'
+		exit 0
+	# else
+	# 	echo 'Wrong deploy key'
+	# 	exit 1
+	# fi
