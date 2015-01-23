13b835b2f2e16528d98b19e91cf7d0ff5d9e37d4
anton <achidyaev@yandex.ru>
2015-01-23 22:14:18 +0300

    remove trash

--- src/actions/HeaderActions.js
+++
@@ -1,9 +0,0 @@
-var AppDispatcher = require('../dispatcher/AppDispatcher'),
-    HeaderConstants = require('../constants/HeaderConstants'),
-    HeaderActions = {
-        menuToggle : function(){
-            AppDispatcher.dispatch({ action : HeaderConstants.ACTION_MENU_TOGGLE });
-        }
-    };
-
-module.exports = HeaderActions;
--- src/actions/MenuActions.js
+++
@@ -1,9 +0,0 @@
-/** @jsx React.DOM */
-'use strict';
-var React = require('react'),
-    Action = require('../action/Action'),
-    MenuStore = require('../stores/MenuStore'),
-    MenuActions = {};
-
-
-module.exports = MenuActions;
