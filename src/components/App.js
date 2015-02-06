eb786d5f93594fcff60505b92bea6e9013ce7816
anton <achidyaev@yandex.ru>
2015-02-06 23:07:51 +0300

    add sign

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,10 @@ var React = require('react'),
             return <div className="app">
                 <Pane />
                 <Toolbar />
+                <div className='sign'>
+                    Images from <a href='https://500px.com' target='_blanc'>500px.com</a><br/>
+                    designed by <a href='https://github.com/chindyaev/react-mosaic' target='_blanc'>chindyaev</a>
+                </div>
             </div>;
         }
     });
